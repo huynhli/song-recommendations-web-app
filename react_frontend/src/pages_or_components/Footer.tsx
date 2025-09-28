e88fa4d91d0013f86137953ceed914f59dc5ca4e
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-09-28 11:34:54 -0400

    fixed footer

--- react_frontend/src/pages_or_components/Footer.tsx
+++ react_frontend/src/pages_or_components/Footer.tsx
@@ -2,7 +2,7 @@ import '../App.css'
 
 function Footer() {
     return (
-        <div className='border-t-1 border-white rounded-2xl max-h-20 py-[4%] px-[4%] text-center flex justify-center items-center'>
+        <div className='border-t-1 border-white rounded-2xl h-auto py-[2%] px-[4%] text-center flex justify-center items-center'>
             <h1 className=''>Thanks for stopping by! If you'd like to learn more about 
                 this project or me, check out my github <a href='https://github.com/huynhli' target='_blank' className='underline text-blue-300 hover:text-blue-500 active:tet-blue-600'>
                 here</a>.</h1>
